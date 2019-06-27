f2e5fbdd2746312a7d9830e197eac2cf8da5e2fd
nik frank <nikbfrank@gmail.com>
2019-06-27 05:32:45 -0700

    build and first two routes

---
+++ build/precache-manifest.96f45af8dc0c8f8cd8e69d7a60759f86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91542d7a47a36458d8b41893d3a1dcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3323285bf09bb3d61b80",
+    "url": "/static/css/2.fcafbfe8.chunk.css"
+  },
+  {
+    "revision": "4125d26f96d6d2accc05",
+    "url": "/static/css/main.ee8610d3.chunk.css"
+  },
+  {
+    "revision": "3323285bf09bb3d61b80",
+    "url": "/static/js/2.6207a247.chunk.js"
+  },
+  {
+    "revision": "4125d26f96d6d2accc05",
+    "url": "/static/js/main.3d8271f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a2018c34d7e9616fe4548c7a4b334f6",
+    "url": "/static/media/goldRecord.6a2018c3.png"
+  }
+]);
